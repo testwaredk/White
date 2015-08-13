7132f42d67d56f05bc0285e8b466a2b1a7094936
jakobojvind <jakob.ojvind@gmail.com>
2015-08-13 12:19:48 +0200

    Removed silverlight from being tested, since the build troubles getting IE 11 installed

--- run-uitests.cmd
+++ run-uitests.cmd
@@ -1,7 +1,7 @@
 @echo off
 IF "%1"=="" goto usage
 
-"%~dp0tools\xUnit\xunit.console.exe" "src\TestStack.White.Modules.Silverlight.UITests\bin\Debug\TestStack.White.Modules.Silverlight.UITests.dll" /nunit "%1-1.xml"
+REM "%~dp0tools\xUnit\xunit.console.exe" "src\TestStack.White.Modules.Silverlight.UITests\bin\Debug\TestStack.White.Modules.Silverlight.UITests.dll" /nunit "%1-1.xml"
 "%~dp0tools\xUnit\xunit.console.exe" "src\TestStack.White.Modules.Win32.UITests\bin\Debug\TestStack.White.Modules.Win32.UITests.dll" /nunit "%1-2.xml"
 "%~dp0tools\xUnit\xunit.console.exe" "src\TestStack.White.Modules.WinForm.UITests\bin\Debug\TestStack.White.Modules.WinForm.UITests.dll" /nunit "%1-3.xml"
 "%~dp0tools\xUnit\xunit.console.exe" "src\TestStack.White.Modules.Wpf.UITests\bin\Debug\TestStack.White.Modules.Wpf.UITests.dll" /nunit "%1-4.xml"
